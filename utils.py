8d5dff9df92dec27d743b040cf123bf4e406bc38
gitterra <nejronki.backup@gmail.com>
2021-11-12 23:34:19 +0300

    update

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from termcolor import colored
 class Keywords():
-    def __init__(self, vars):
+    def __init__(self, vars_):
         self.builtInFunctions = set(['abs',       'aiter',    'all',          'any',         'anext',      'ascii',   'bin',        'bool',       'breakpoint', 'bytearray',
                                      'bytes',     'callable', 'chr',          'classmethod', 'compile',    'complex', 'delattr',    'dict',       'dir',        'divmod',
                                      'enumerate', 'eval',     'exec',         'filter',      'float',      'format',  'frozenset',  'getattr',    'globals',    'hasattr',
@@ -8,7 +8,7 @@ class Keywords():
                                      'list',      'locals',   'map',          'max',         'memoryview', 'min',     'next',       'object',     'oct',        'open', 
                                      'ord',       'pow',      'print',        'property',    'range',      'repr',    'reversed',   'round',      'set',        'setattr', 
                                      'slice',     'sorted',   'staticmethod', 'str',         'sum',        'super',   'tuple',      'type',       'vars',       'zip'])
-        self.variables = vars
+        self.variables = vars_
         self.error = colored('Ошибка. Вы создали одну или несколько переменных с именем встроенной функции:', color='red', attrs=['bold'])
         self.information = '\nИсправьте имена переменных и повторите попытку' \
                             +'\n\x1B[3mПочему нельзя использовать имена встроенных функций в качестве переменных:\x1B[3m \n' \
@@ -17,8 +17,8 @@ class Keywords():
     def check(self):
         intersection_func = self.builtInFunctions & set(self.variables)
         if len(intersection_func) > 0:
-            print(self.error)            
+            print(self.error)          
             for func in intersection_func:
                 print(colored(f'  {func}', attrs=['bold']))
-                del globals()[func]
+                del self.variables[func]
             print(self.information)
