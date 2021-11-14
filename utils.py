9ac346ad8fdc44f7d8ea26863fc867fe01d1ce52
gitterra <nejronki.backup@gmail.com>
2021-11-14 12:10:00 +0300

    update

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from termcolor import colored
+from IPython.core.display import HTML
 class Keywords():
     def __init__(self, vars_):
         self.builtInFunctions = set(['abs',       'aiter',    'all',          'any',         'anext',      'ascii',   'bin',        'bool',       'breakpoint', 'bytearray',
@@ -9,7 +9,7 @@ class Keywords():
                                      'ord',       'pow',      'print',        'property',    'range',      'repr',    'reversed',   'round',      'set',        'setattr', 
                                      'slice',     'sorted',   'staticmethod', 'str',         'sum',        'super',   'tuple',      'type',       'vars',       'zip'])
         self.variables = vars_
-        self.error = colored('Ошибка. Вы создали одну или несколько переменных с именем встроенной функции:', color='red', attrs=['bold'])
+        self.error = '<p><b><font color="#880000">Ошибка. Вы создали одну или несколько переменных с именем встроенной функции:</font></b></p>'
         self.information = '<p>Исправьте имена переменных и повторите попытку <br> <i>\
                             Почему нельзя использовать имена встроенных функций в качестве переменных:</i>\
                             </p><a href="https://colab.research.google.com/drive/1kUQVE_vTJZEiNJ5yt0Zgg0MRVYslY4b4?usp=sharing"\
@@ -18,10 +18,10 @@ class Keywords():
     def check(self) -> bool:
         intersection_func = self.builtInFunctions & set(self.variables)
         if len(intersection_func) > 0:
-            print(self.error)          
+            list_func = ''
             for func in intersection_func:
-                print(colored(f'  {func}', attrs=['bold']))
+                list_func += f'<b>&nbsp;&nbsp;&nbsp;&nbsp;{func}</b><br>'
                 del self.variables[func]
-            print(self.information)
+            HTML(self.error + list_func + self.information)
             return False
         return True
