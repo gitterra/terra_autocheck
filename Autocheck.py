0bf99509245c4e920c2b996d46667eb79f91ef0b
gitterra <80519167+gitterra@users.noreply.github.com>
2021-11-25 16:33:35 +0300

    Update Autocheck.py

--- Autocheck.py
+++ Autocheck.py
@@ -27,6 +27,90 @@ def StartHomework(self):
         baseblock.lessontwo.pro.Start(user)
     elif user.HW_ID == 1745:
         baseblock.lessontwo.ultraPro.Start(user)
+    elif user.HW_ID == 1747:
+        baseblock.lessonthree.light.Start(user)
+    elif user.HW_ID == 1748:
+        baseblock.lessonthree.pro.Start(user)
+    elif user.HW_ID == 1749:
+        baseblock.lessonthree.ultraPro.Start(user)
+    elif user.HW_ID == 1751:
+        baseblock.lessonfour.light.Start(user)
+    elif user.HW_ID == 1752:
+        baseblock.lessonfour.pro.Start(user)
+    elif user.HW_ID == 1753:
+        baseblock.lessonfour.ultraPro.Start(user)
+    elif user.HW_ID == 1755:
+        baseblock.lessonfive.light.Start(user)
+    elif user.HW_ID == 1756:
+        baseblock.lessonfive.pro.Start(user)
+    elif user.HW_ID == 1757:
+        baseblock.lessonfive.ultraPro.Start(user)
+    elif user.HW_ID == 1759:
+        baseblock.lessonsix.light.Start(user)
+    elif user.HW_ID == 1760:
+        baseblock.lessonsix.pro.Start(user)
+    elif user.HW_ID == 1761:
+        baseblock.lessonsix.ultraPro.Start(user)
+    elif user.HW_ID == 1763:
+        baseblock.lessonseven.light.Start(user)
+    elif user.HW_ID == 1764:
+        baseblock.lessonseven.pro.Start(user)
+    elif user.HW_ID == 1765:
+        baseblock.lessonseven.ultraPro.Start(user)
+    elif user.HW_ID == 1767:
+        baseblock.lessoneight.light.Start(user)
+    elif user.HW_ID == 1768:
+        baseblock.lessoneight.pro.Start(user)
+    elif user.HW_ID == 1769:
+        baseblock.lessoneight.ultraPro.Start(user)
+    elif user.HW_ID == 1771:
+        baseblock.lessonnine.light.Start(user)
+    elif user.HW_ID == 1772:
+        baseblock.lessonnine.pro.Start(user)
+    elif user.HW_ID == 1773:
+        baseblock.lessonnine.ultraPro.Start(user)
+    elif user.HW_ID == 1775:
+        baseblock.lessonten.light.Start(user)
+    elif user.HW_ID == 1776:
+        baseblock.lessonten.pro.Start(user)
+    elif user.HW_ID == 1777:
+        baseblock.lessonten.ultraPro.Start(user)
+    elif user.HW_ID == 1779:
+        baseblock.lessoneleven.light.Start(user)
+    elif user.HW_ID == 1780:
+        baseblock.lessoneleven.pro.Start(user)
+    elif user.HW_ID == 1781:
+        baseblock.lessoneleven.ultraPro.Start(user)
+    elif user.HW_ID == 1783:
+        baseblock.lessontwelve.light.Start(user)
+    elif user.HW_ID == 1784:
+        baseblock.lessontwelve.pro.Start(user)
+    elif user.HW_ID == 1785:
+        baseblock.lessontwelve.ultraPro.Start(user)
+    elif user.HW_ID == 1787:
+        baseblock.lessonthirteen.light.Start(user)
+    elif user.HW_ID == 1788:
+        baseblock.lessonthirteen.pro.Start(user)
+    elif user.HW_ID == 1789:
+        baseblock.lessonthirteen.ultraPro.Start(user)
+    elif user.HW_ID == 1791:
+        baseblock.lessonfourteen.light.Start(user)
+    elif user.HW_ID == 1792:
+        baseblock.lessonfourteen.pro.Start(user)
+    elif user.HW_ID == 1793:
+        baseblock.lessonfourteen.ultraPro.Start(user)
+    elif user.HW_ID == 1795:
+        baseblock.lessonfifteen.light.Start(user)
+    elif user.HW_ID == 1796:
+        baseblock.lessonfifteen.pro.Start(user)
+    elif user.HW_ID == 1797:
+        baseblock.lessonfifteen.ultraPro.Start(user)
+    elif user.HW_ID == 1799:
+        baseblock.lessonsixteen.light.Start(user)
+    elif user.HW_ID == 1800:
+        baseblock.lessonsixteen.pro.Start(user)
+    elif user.HW_ID == 1801:
+        baseblock.lessonsixteen.ultraPro.Start(user)
     
         
 def Start(homework_id, content):    
