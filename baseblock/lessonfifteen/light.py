172287863cbd2bdf6d1762b429b2d8005f8ea244
terragit <seregaak3@gmail.com>
2021-11-25 16:12:08 +0300

    'update'

---
+++ baseblock/lessonfifteen/light.py
@@ -0,0 +1,36 @@
+# Homework ID 1795
+from IPython import display as dsp
+from ... import utils
+
+'''
+
+Здесь Ваши функции автопроверки
+
+'''
+
+
+
+#
+# ОБЯЗАТЕЛЬНЫЙ БЛОК
+#
+
+def check_homework(self):
+    dsp.clear_output(wait=True)
+    global user
+
+    '''
+    
+    Здесь код вызова функций автопроверки ДЗ
+    
+    '''
+
+    # Проверка на пересечение имён
+    # Пока оформляем это в качестве рекомендации (НЕ ОШИБКА)
+    # Просто оставляйте этот код здесь
+    keywords = utils.Keywords(user.content)
+    keywords.check()
+
+def Start(_user):
+    global user
+    user = _user
+    check_homework(None)
